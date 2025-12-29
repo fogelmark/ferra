c9a4c5277e9560dd6936f675ded21b3998136716
fogelmark <108455196+fogelmark@users.noreply.github.com>
2025-12-29 19:31:16 +0100

    Added landing page + package setup

---
+++ lib/fonts.ts
@@ -0,0 +1,11 @@
+import localFont from "next/font/local"
+
+export const murmure = localFont({
+  src: [
+    {
+      path: "../public/fonts/LeMurmure-Regular.woff2",
+      weight: "400",
+      style: "normal",
+    },
+  ],
+})
