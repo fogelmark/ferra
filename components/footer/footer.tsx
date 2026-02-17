6bfee5cb85ef06ba17935fc501e16372555554ae
fogelmark <108455196+fogelmark@users.noreply.github.com>
2026-02-17 20:26:51 +0100

    Added basic footer layout

--- components/footer/footer.tsx
+++ components/footer/footer.tsx
@@ -1,5 +1,7 @@
 "use client"
 
+import { leaguegothic } from "@/lib/fonts"
+import { cn } from "@/lib/utils"
 import { motion, useTransform, useScroll } from "motion/react"
 import { useRef } from "react"
 
@@ -8,28 +10,42 @@ export default function Footer() {
 
 	const { scrollYProgress } = useScroll({
 		target: containerRef,
+		// We track from when the top of the footer hits the bottom of the viewport
+		// until the bottom of the footer hits the bottom of the viewport.
 		offset: ["start end", "end end"],
 	})
 
-	const y = useTransform(scrollYProgress, [0, 1], ["200px", "0px"])
+	// Adjusted the parallax transform to start further down for a 100vh height
+	const y = useTransform(scrollYProgress, [0, 1], ["300px", "0px"])
 
 	return (
 		<div
 			ref={containerRef}
-			className="relative h-[700px] bg-white"
+			className="relative h-screen bg-[#e7e7e7]"
 			style={{
 				clipPath: "polygon(0% 0, 100% 0%, 100% 100%, 0 100%)",
 			}}
 		>
-			<div className="relative -top-[100vh] h-[calc(100vh+700px)]">
-				<div className="sticky top-[calc(100vh-700px)] h-[700px]">
+			<div className="relative -top-[100vh] h-[200vh]">
+				<div className="sticky top-0 h-screen overflow-hidden">
 					<motion.div
 						style={{ y }}
-						className="relative flex h-full w-full items-center justify-center px-4 py-8"
+						className="relative grid h-full w-full grid-cols-12 px-4 py-4"
 					>
-						<h1 className="text-8xl text-ash-gray">
-							FOOTER
-						</h1>
+						<motion.h1
+							className={cn(
+								"text-ash-gray text-center text-[25.5vw] leading-[80%] whitespace-nowrap uppercase select-none",
+								leaguegothic.className,
+							)}
+						>
+							ferra studio
+						</motion.h1>
+						<div className="h-min-content col-span-12 flex items-end justify-between text-sm text-[#8c8c8c]">
+							<p className="uppercase">© 2026 Ferra Studio</p>
+							<p>hello@ferrastudio.com</p>
+							{/* <p>11:35 AM Stockholm</p> */}
+							<p className="uppercase">Back to top</p>
+						</div>
 					</motion.div>
 				</div>
 			</div>
