259a51ae00a0d8f162e147ec6e19a824e47bc404
fogelmark <108455196+fogelmark@users.noreply.github.com>
2026-03-02 16:53:09 +0100

    Added will-change-transform to hero headings

--- components/hero.tsx
+++ components/hero.tsx
@@ -27,14 +27,14 @@ export default function Hero() {
 			ref={ref}
 			className="text-bone-white bg-ash-gray relative isolate z-10 grid h-dvh w-full grid-cols-4 grid-rows-[auto_min-content] gap-y-4 px-4 py-14 md:h-screen md:gap-y-8 md:py-0"
 		>
-			<div className="relative z-10 col-span-4 self-end md:col-span-2 md:col-start-3 md:pl-4">
+			<div className="relative z-10 col-span-4 self-end will-change-transform md:col-span-2 md:col-start-3 md:pl-4">
 				<motion.div
 					style={{ y: headingTwo }}
 					initial={{ opacity: 0, y: "-50%" }}
 					animate={{ opacity: 1, y: 0 }}
 					transition={{
-						duration: 1.8,
-						delay: 1.7,
+						duration: 1.85,
+						delay: 1.8,
 						ease: [0.16, 1, 0.3, 1],
 					}}
 				>
@@ -47,7 +47,7 @@ export default function Hero() {
 				</motion.div>
 			</div>
 
-			<div className="relative col-span-4 self-end max-sm:hidden">
+			<div className="relative col-span-4 self-end will-change-transform max-sm:hidden">
 				<motion.h1
 					initial={{ opacity: 0, y: "-30%" }}
 					animate={{ opacity: 1, y: 0 }}
